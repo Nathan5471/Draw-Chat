d6f5036757d5047bae8a0c87dd227e85ac3006c6
Nathan Cloutier <nathanc4571@gmail.com>
2025-10-19 10:48:14 -0400

    Add basic message creation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     unreadMessages: number;
   }
   const [chats, setChats] = useState<Chat[]>([]);
-  const [selectedChat, setSelectedChat] = useState<number | null>(null); // The chat id
+  const [selectedChat, setSelectedChat] = useState<Chat | null>(null);
   interface Message {
     id: number;
     content: string;
@@ -26,6 +26,19 @@ export default function Home() {
   const [selectedChatMessages, setSelectedChatMessages] = useState<Message[]>(
     []
   );
+  const [message, setMessage] = useState(Array(25).fill("white"));
+  const [selectedColor, setSelectedColor] = useState<
+    | "white"
+    | "gray"
+    | "black"
+    | "red"
+    | "orange"
+    | "yellow"
+    | "green"
+    | "blue"
+    | "cyan"
+    | "purple"
+  >("white");
 
   useEffect(() => {
     socket.emit("getUserChats");
@@ -37,7 +50,7 @@ export default function Home() {
     socket.on(
       "chatMessages",
       (data: { chatId: number; messages: Message[] }) => {
-        if (data.chatId !== selectedChat) {
+        if (data.chatId !== selectedChat?.id) {
           return;
         }
         setSelectedChatMessages(data.messages);
@@ -47,7 +60,7 @@ export default function Home() {
     socket.on(
       "messageSent",
       (data: { chatId: number; message: { id: number; user: string } }) => {
-        if (selectedChat !== data.chatId) {
+        if (selectedChat?.id !== data.chatId) {
           // TODO: Implement notification with Toasts or something
           return;
         }
@@ -61,7 +74,11 @@ export default function Home() {
 
     socket.on("chatCreated", (data: { id: number }) => {
       closeOverlay();
-      setSelectedChat(data.id);
+      const chat = chats.filter((chat) => chat.id === data.id);
+      if (chat.length === 0) {
+        return;
+      }
+      setSelectedChat(chat[0]);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -70,10 +87,11 @@ export default function Home() {
     if (!chats.some((chat) => chat.id === id)) {
       return;
     }
-    if (selectedChat === id) {
+    if (selectedChat?.id === id) {
       return;
     }
-    setSelectedChat(id);
+    const chat = chats.filter((chat) => chat.id === id);
+    setSelectedChat(chat[0]);
     setSelectedChatMessages([]);
     socket.emit("getChatMessages", { chadId: id });
   };
@@ -109,7 +127,7 @@ export default function Home() {
               <div
                 key={chat.id}
                 className={`${
-                  selectedChat === chat.id
+                  selectedChat?.id === chat.id
                     ? "bg-primary-a0 hover:bg-primary-a1"
                     : "bg-surface-a3 hover:bg-surface-a4"
                 } hover:scale-105 transition-all m-2 rounded-lg p-2`}
@@ -123,7 +141,82 @@ export default function Home() {
             ))
           )}
         </div>
-        <div className="h-full w-4/5 flex items-center justify-center"></div>
+        <div
+          className={`h-full w-4/5 flex ${
+            !selectedChat && "items-center justify-center"
+          }`}
+        >
+          {selectedChat ? (
+            <div className="w-full h-full p-2 flex flex-col">
+              <h3 className="text-2xl text-center font-bold">{`${selectedChat.users
+                .map((user) => user.username)
+                .join(", ")}`}</h3>
+              <div className="w-full mt-auto bg-surface-a1 rounded-lg m-2 p-2 flex flex-row">
+                <div className="flex flex-col w-1/4">
+                  <p className="text-center text-lg">Select a color</p>
+                  <div className="grid grid-cols-5 gap-y-2">
+                    {[
+                      "white",
+                      "gray",
+                      "black",
+                      "red",
+                      "orange",
+                      "yellow",
+                      "green",
+                      "blue",
+                      "cyan",
+                      "purple",
+                    ].map((color) => (
+                      <div
+                        key={color}
+                        className={`w-10 h-10 rounded hover:scale-105 transition-transform ${
+                          selectedColor === color &&
+                          "border-surface-a3 border-4"
+                        }`}
+                        style={{ backgroundColor: color }}
+                        onClick={() =>
+                          setSelectedColor(
+                            color as
+                              | "white"
+                              | "gray"
+                              | "black"
+                              | "red"
+                              | "orange"
+                              | "yellow"
+                              | "green"
+                              | "blue"
+                              | "cyan"
+                              | "purple"
+                          )
+                        }
+                      />
+                    ))}
+                  </div>
+                </div>
+                <div className="flex flex-col w-3/4 items-center justify-center">
+                  <div className="grid grid-cols-5 gap-0.5">
+                    {message.map((color, index) => (
+                      <div
+                        key={index}
+                        className="w-5 h-5"
+                        style={{ backgroundColor: color }}
+                        onClick={() => {
+                          setMessage((prevMessage) => {
+                            const newMessage = [...prevMessage];
+                            newMessage[index] = selectedColor;
+                            return newMessage;
+                          });
+                        }}
+                      ></div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p className="text-2xl">Select a chat</p>
+          )}
+        </div>
       </div>
     </div>
   );
