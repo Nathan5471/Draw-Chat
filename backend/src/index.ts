ba8e3c2bd45511ffa6a2520e80be87bcbe8c0e0e
Nathan Cloutier <nathanc4571@gmail.com>
2025-10-19 20:38:57 -0400

    Deploy with Docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRouter";
 import messageSocket from "./socket/messageSocket";
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
 
@@ -24,13 +23,16 @@ app.use("/api/auth", authRouter);
 
 messageSocket(io);
 
-app.use(
-  "/",
-  createProxyMiddleware({
-    target: "http://localhost:5173",
-    changeOrigin: true,
-  })
-);
+app.use(express.static("public"));
+
+app.use((req: any, res: any) => {
+  res.sendFile("./public/index.html", { root: "." }, (error: any) => {
+    if (error) {
+      console.error("Error sending index.html:", error);
+      res.status(500).send("Error loading index.html");
+    }
+  });
+});
 
 server.listen(3000, () => {
   console.log("Server listening on port 3000");
